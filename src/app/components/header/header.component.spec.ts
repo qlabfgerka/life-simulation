2875bf6f37cc096c2be0b070a03e3a52f4d01a1f
Jan Premzl <jan.premzl16@gmail.com>
2023-05-06 17:30:26 +0200

    Prepare unit testing

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +9,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      imports: [MatToolbarModule],
+      declarations: [HeaderComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/components/simulations/aggresive-simulation/aggresive-simulation.component.spec.ts
+++ src/app/components/simulations/aggresive-simulation/aggresive-simulation.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AggresiveSimulationComponent } from './aggresive-simulation.component';
+import { BarChartModule } from 'src/app/shared/charts/bar-chart/bar-chart.module';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AggresiveSimulationComponent', () => {
   let component: AggresiveSimulationComponent;
@@ -8,9 +13,9 @@ describe('AggresiveSimulationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AggresiveSimulationComponent ]
-    })
-    .compileComponents();
+      imports: [BrowserAnimationsModule, BarChartModule, FormsModule, MatFormFieldModule, MatInputModule],
+      declarations: [AggresiveSimulationComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AggresiveSimulationComponent);
     component = fixture.componentInstance;
